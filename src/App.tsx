baa9f94aa01a48c2fb5e1dcbb7b55fb1602c5646
MIMBOL228 <aekemerov@yandex.ru>
2024-01-26 11:10:05 +0500

    Правка Index и добавление Schedule
    Signed-off-by: MIMBOL228 <aekemerov@yandex.ru>

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Main} from "./pages/Main/Main.tsx";
 import React from "react";
 import "./App.scss"
+import {Schedule} from "./pages/Schedule/Schedule.tsx";
 
 function App() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Main/>}/>]
+                    <Route path="/" element={<Main/>}/>
+                    <Route path="/schedule" element={<Schedule/>}/>
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/pages/Schedule/Schedule.tsx
@@ -0,0 +1,9 @@
+import "./Schedule.scss"
+
+export const Schedule = () => {
+    return (
+        <div className="schedule_page">
+
+        </div>
+    );
+};
